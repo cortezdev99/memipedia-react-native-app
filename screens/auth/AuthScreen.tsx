6dcd2575b25ff157dedfa01e60af4763b9a83840
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-07 17:42:34 -0600

    added some basic styles and passed props to text input to get rid of autoCapitalize and spellCheck specifically because its an email input

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { dark } from '../../styles/colors';
 
 export default () => {
   const [formToShow, setFormToShow] = useState("LOGIN");
@@ -30,19 +31,27 @@ export default () => {
   }
 
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text>{handleHeaderText()}</Text>
+    <View style={{ marginTop: 100, backgroundColor: dark, height: "100%" }}>
+      <Text style={{ color: "white" }}>{handleHeaderText()}</Text>
 
       <View style={{ marginTop: 20, marginBottom: 20 }}>
         <TextInput
           placeholder='Email'
           value={email}
           onChangeText={(val) => setEmeail(val)}
+          style={{
+            backgroundColor: "white",
+            borderRadius: 20,
+            height: 40,
+            paddingLeft: 20,
+          }}
+          autoCapitalize="none"
+          spellCheck={false}
         />
       </View>
 
       <TouchableOpacity onPress={handleAuthTypePress}>
-        <Text>{screenTypeText()}</Text>
+        <Text style={{ color: "white" }}>{screenTypeText()}</Text>
       </TouchableOpacity>
     </View>
   )
