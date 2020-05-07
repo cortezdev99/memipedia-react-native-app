94f88d8d9c303d958609d195f1038a57899a5d53
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-07 17:17:07 -0600

    I'm using react hooks to dynamically render content

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 export default () => {
+  const [formToShow, setFormToShow] = useState("LOGIN");
+
+  const screenTypeText = () => {
+    if (formToShow === "LOGIN") {
+      return "Need an account? Register";
+    } else if (formToShow === "REGISTER") {
+      return "Already have an account? Login"
+    }
+  }
+
+  const handleAuthTypePress = () => {
+    if (formToShow === "LOGIN") {
+      setFormToShow("REGISTER")
+    } else if (formToShow === "REGISTER") {
+      setFormToShow("LOGIN")
+    }
+  }
+
+  const handleHeaderText = () => {
+    if (formToShow === "LOGIN") {
+      return "Login"
+    } else if (formToShow === "REGISTER") {
+      return "Register"
+    }
+  }
+
   return (
     <View style={{ marginTop: 100 }}>
-      <Text>Login</Text>
+      <Text>{handleHeaderText()}</Text>
 
-      <TouchableOpacity>
-        <Text>don't have an account? Register</Text>
+      <TouchableOpacity onPress={handleAuthTypePress}>
+        <Text>{screenTypeText()}</Text>
       </TouchableOpacity>
     </View>
   )
