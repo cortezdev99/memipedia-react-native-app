ab874c4019fe7afd6e546767b089fb8ce07ae89c
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-07 14:21:27 -0600

    organized my stylesheets

---
+++ styles/navigation/bottomTabStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    paddingTop: 15,
+    paddingBottom: 15
+  }
+})
+
+export default styles
