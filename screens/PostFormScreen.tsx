ad397cc3f96a4e857a891c17ae44578a29b9373c
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-09 15:32:33 -0600

    working with user permissions now to access the camera roll and successfully get an image

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import PostImagePicker from '../components/posts/PostImagePicker'
+
 const PostFormScreen = () => {
   return (
-    <View>
+    <View style={{ height: "100%" }}>
       <Text>Post Form Screen</Text>
+
+      <View style={{ marginTop: 40, height: 100 }}>
+        <PostImagePicker />
+      </View>
     </View>
   )
 }
