783e40bab2d9030ed3ab7e425b8f04a7655247d3
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-08 20:07:03 -0600

    Created the ability for users to sign out

--- screens/AccountScreen.tsx
+++ screens/AccountScreen.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
+import * as SecureStore from 'expo-secure-store'
 
-const AccountScreen = () => {
+import CurrentUserContext from '../contexts/CurrentUserContext'
+import Button from '../components/helpers/Button';
+
+
+interface IAccountScreenProps {
+  navigation: {
+    navigate: (arg: string) => void;
+  }
+}
+
+const AccountScreen = (props: IAccountScreenProps) => {
+  const { setCurrentUser } = useContext(CurrentUserContext)
+
+  const handleSignOut = async () => {
+    await SecureStore.deleteItemAsync("memipedia_secure_token")
+    setCurrentUser(null);
+    props.navigation.navigate("Auth")
+  }
+
   return (
     <View>
       <Text>Account Screen</Text>
+
+      <View style={{ marginTop: 20 }}>
+        <Button onPress={handleSignOut} text="Sign Out" />
+      </View>
     </View>
   )
 }
