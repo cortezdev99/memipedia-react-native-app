543e065acfeb5b47a6362e497546016bcd1c73d7
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-09 12:55:49 -0600

    Implemented the api call for post data

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import * as SecureStore from 'expo-secure-store'
 
 import Container from '../components/layouts/Container'
+import api from '../utils/api';
 
 interface IFeedScreenProps {
   navigation: {
@@ -10,9 +12,33 @@ interface IFeedScreenProps {
 }
 
 const FeedScreen = (props: IFeedScreenProps) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    const token = await SecureStore.getItemAsync("memipedia_secure_token");
+
+    api.get("memipedia_posts", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(resp => {
+      setPosts(resp.data.memipedia_posts)
+    }).catch(err => {
+      console.log("err from posts", err)
+    })
+  }
+
   return (
     <Container navigate={props.navigation.navigate}>
       <Text>Feed Screen</Text>
+
+      <View style={{marginTop: 20}}>
+        <Text>{JSON.stringify(posts)}</Text>
+      </View>
     </Container>
   )
 }
