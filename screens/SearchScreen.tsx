f8923a7ac0e094edcbfb4a380e8dd9bdb51f9907
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-11 15:11:37 -0600

    Created the initial search bar

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import Container from '../components/layouts/Container'
 
@@ -10,9 +10,31 @@ interface ISearchScreenProps {
 }
 
 const SearchScreen = (props: ISearchScreenProps) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (query: any) => {
+    console.log("Searching for", query)
+  }
+
+  const searchBar = (
+    <View>
+      <TextInput
+        value={query}
+        onChangeText={val => setQuery(val)}
+        placeholderTextColor="white"
+        placeholder="Search"
+        onSubmitEditing={() => handleSearch(query)}
+      />
+
+      <TouchableOpacity onPress={() => handleSearch(query)}>
+        <Text style={{ color: "white" }}>Search</Text>
+      </TouchableOpacity>
+    </View>
+  )
   return (
     <Container navigate={props.navigation.navigate}>
       <Text>Search Screen</Text>
+      {searchBar}
     </Container>
   )
 }
